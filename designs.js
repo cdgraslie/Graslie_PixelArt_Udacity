15c78a501f2c28f674de965b16ec6d6f0785f3e4
cdgraslie <102864835+cdgraslie@users.noreply.github.com>
2022-04-04 15:21:44 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,33 +1,29 @@
 //** Define variables using id's from HTML doc.
-var canvas = document.getElementById("pixelCanvas");
-var color = document.getElementById("colorPicker");
-var sizePicker = document.getElementById("sizePicker");
+var grid = document.getElementById("pixelCanvas");
 var height = document.getElementById("inputHeight");
 var width = document.getElementById("inputWidth");
+var colorPicker = document.getElementById("colorPicker");
+var sizePicker = document.getElementById("sizePicker");
 
 //** Create function to generate grid when user clicks submit.
-sizePicker.addEventListener("submit", function(evt) {
-    evt.preventDefault();
-      if (canvas.hasChildNodes()) {
+sizePicker.addEventListener("submit", function(e) {
+    e.preventDefault();
+      if (grid.hasChildNodes()) {
         removeChild();
     }
     makeGrid(height.value, width.value);
 });
-//** Create grid function with listeners for coloring cells
-//** with mouse clicks and preventing issues with right-clicks.
+
+//** Create grid function with listeners for coloring cells with mouse clicks.
 function makeGrid(height, width) {
     for (let y = 0; y < height; y++) {
-        let row = canvas.insertRow(y);
+        let row = grid.insertRow(y);
         for (let x = 0; x < width; x++) {
             let cell = row.insertCell(x);
-            cell.addEventListener("mousedown", function(evt) {
+            cell.addEventListener("mousedown", function(e) {
                 cell.style.backgroundColor = colorPicker.value;
-            cell.addEventListener("contextmenu", function(evt) {
-              evt.preventDefault();
-            } )
             } )
         }
     }
 }
-
 
